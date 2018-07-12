6a50c8195c4e8eb74564201938b77b206e690977
NicklasMCHD <NicklasMCHD@live.dk>
2018-07-12 20:52:54 +0200

    Updated the xample

--- example.py
+++ example.py
@@ -1,8 +1,16 @@
 # This is an example for how to use the resource manager.
+# For this example to work, the iv and compression_level must not have been changed.
 
+# first import it.
 from resourcemanager import *
 
+# Make an instance of the resource manager.
 ress = ResourceManager()
+# Load the resources packaget in "resources.dat" with the encryption key "test123"
 ress.loadResources("resources.dat", "test123")
+
+# Get the resource "test.txt" from the loaded resources.
 txt = ress.getResource("test.txt")
+
+# print the result.
 print(txt)
